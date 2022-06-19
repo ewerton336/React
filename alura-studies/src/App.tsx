4220a851995e398bcd0d5da1e599b6ef82b6f1ab
Ewerton  Guimarães <ewertonpg@hotmail.com>
2022-06-19 19:18:37 -0300

    início das aulas

---
+++ alura-studies/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Botao from './components/Botao';
+
+function App() {
+  return (
+    <div className="App">
+    <Botao/>
+    </div>
+  );
+}
+
+export default App;
---
+++ alura-studies/src/Formulario/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Formulario extends React.Component {
+    render() {
+        return (
+            <form>
+<div>
+
+</div>
+<div>
+    
+</div>
+            </form>
+        )
+    }
+}
