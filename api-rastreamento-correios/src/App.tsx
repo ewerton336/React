9cec188b2b9b9365c0786d21153e552b161a7cb0
Ewerton  Guimarães <ewertonpg@hotmail.com>
2022-09-28 15:46:02 -0300

    consumação de api

--- api-rastreamento-correios/src/App.tsx
+++ api-rastreamento-correios/src/App.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import api from "./components/axios";
 
 function App() {
-  var results = teste().then;
-  console.log(results);
+  var result = teste();
+  console.log(result);
 
+  console.log("passei por aqui ");
   return <div>Oiiii ;</div>;
 }
 
 async function teste() {
-  var response = await api.get("");
-  return response.data;
+  var response = await api.get("LB317268015HK");
+  return await response.data;
 }
 
 export default App;
--- api-rastreamento-correios/src/components/axios/index.tsx
+++ api-rastreamento-correios/src/components/axios/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://proxyapp.correios.com.br/v1/sro-rastro/LB317268015HK",
+  baseURL: "https://proxyapp.correios.com.br/v1/sro-rastro/",
 });
 
 export default api;
